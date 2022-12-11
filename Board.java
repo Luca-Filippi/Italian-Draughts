c10a7384aec1b51651d88539e3b65829374170a3
Mauro Farina <maurofarina@protonmail.com>
2022-12-11 13:12:10 +0100

    add static class DisplayBoard

--- Board.java
+++ Board.java
@@ -1,52 +1,52 @@
 public class Board {
 
-    private Square[][] board;
+    private Square[][] boardSquares;
 
     public Board(){
-        board = new Square[8][8];
+        boardSquares = new Square[8][8];
         for(byte i=0; i<8; i++){
             for(byte j=0; j<8; j++){
-                board[i][j] = new Square(i, j, SquareContent.EMPTY);
+                boardSquares[i][j] = new Square(i, j, SquareContent.EMPTY);
             }
             System.out.println();
         }
         initBoard();
-        displayBoard();
     }
 
     private void initBoard(){
+        // add White initial pieces (rows 1, 2, 3)
         for(byte i=0; i<3; i++){
             for(byte j=0; j<8; j+=2){
                 if(i % 2 == 0 && j == 0){
                     j--;
                     continue;
                 }
-                this.board[i][j].updateState(SquareContent.WHITE_MAN);
+                this.boardSquares[i][j].updateState(SquareContent.WHITE_MAN);
             }
         }
+        // add Black initial pieces (rows 6, 7, 8)
         for(byte i=5; i<8; i++){
             for(byte j=0; j<8; j+=2){
                 if(i % 2 == 0 && j == 0){
                     j--;
                     continue;
                 }
-                this.board[i][j].updateState(SquareContent.BLACK_MAN);
+                this.boardSquares[i][j].updateState(SquareContent.BLACK_MAN);
             }
             System.out.println();
         }
     }
 
-    private void displayBoard(){
-        for(byte i=7; i>=0; i--){
-            for(byte j=0; j<8; j++){
-                System.out.print("{"+i+","+j+"}" + board[i][j].toString() + board[i][j].getColor().toString() + "\t");
-                //System.out.print(board[i][j].toString());
-            }
-            System.out.println();
-        }
+    public Square[][] getBoardSquares() {
+        return boardSquares;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // temporary test purpose
         Board b = new Board();
+        System.out.println("White Player turn: ");
+        DisplayBoard.displayBoardForWhitePlayer(b.getBoardSquares());
+        System.out.println();
+        System.out.println("Black Player turn: ");
+        DisplayBoard.displayBoardForBlackPlayer(b.getBoardSquares());
     }
 }
